d66f92ab34d77d66474023042579307dfd5bc0ad
Thanh Thu Cao <katie.caooo@gmail.com>
2021-09-15 15:26:20 +0000

    Add file Account table

---
+++ src/main/java/com/app/source/entities/Account.java
@@ -0,0 +1,19 @@
+package com.app.source.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "account")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+}
