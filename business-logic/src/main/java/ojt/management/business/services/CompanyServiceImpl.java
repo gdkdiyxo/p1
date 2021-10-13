6ae76b7cd15e519a4293710b2e49e061b7c6b7c9
Thanh Thu Cao <katie.caooo@gmail.com>
2021-10-13 14:09:54 +0700

    Authen company

--- business-logic/src/main/java/ojt/management/business/services/CompanyServiceImpl.java
+++ business-logic/src/main/java/ojt/management/business/services/CompanyServiceImpl.java
@@ -2,9 +2,11 @@ package ojt.management.business.services;
 
 import ojt.management.common.exceptions.CompanyNotExistedException;
 import ojt.management.common.payload.request.CompanyUpdateRequest;
+import ojt.management.data.entities.Account;
 import ojt.management.data.entities.Company;
 import ojt.management.data.repositories.AccountRepository;
 import ojt.management.data.repositories.CompanyRepository;
+import ojt.management.data.repositories.RepresentativeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,10 +16,14 @@ public class CompanyServiceImpl implements CompanyService {
 
     private final CompanyRepository companyRepository;
     private final AccountRepository accountRepository;
+    private final RepresentativeRepository representativeRepository;
 
-    public CompanyServiceImpl(CompanyRepository companyRepository, AccountRepository accountRepository) {
+    public CompanyServiceImpl(CompanyRepository companyRepository,
+                              AccountRepository accountRepository,
+                              RepresentativeRepository representativeRepository) {
         this.companyRepository = companyRepository;
         this.accountRepository = accountRepository;
+        this.representativeRepository = representativeRepository;
     }
 
     @Override
@@ -39,5 +45,21 @@ public class CompanyServiceImpl implements CompanyService {
         company.setDescription(companyUpdateRequest.getDescription());
 
         return companyRepository.save(company);
+    }
+
+    @Override
+    public Company getCompanyById(Long id, Long accountId) throws CompanyNotExistedException {
+        Account account = accountRepository.getById(accountId);
+        if (account.getRepresentative() != null) { //The Rep only get their company
+            Long repCompanyId = representativeRepository.getById(accountId).getCompany().getId();
+            if (id != repCompanyId) {
+                throw new CompanyNotExistedException();
+            }
+        } else { //The other role can get all
+            if (Boolean.FALSE.equals(companyRepository.existsById(id))) {
+                throw new CompanyNotExistedException();
+            }
+        }
+        return companyRepository.getById(id);
     }
 }
