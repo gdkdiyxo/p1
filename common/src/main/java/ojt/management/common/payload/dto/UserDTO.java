8f95973c8f49391d53bd5b5eaea77a64678d7252
Thanh Thu Cao <katie.caooo@gmail.com>
2021-10-13 23:57:44 +0700

    clean code

--- common/src/main/java/ojt/management/common/payload/dto/UserDTO.java
+++ common/src/main/java/ojt/management/common/payload/dto/UserDTO.java
@@ -3,7 +3,6 @@ package ojt.management.common.payload.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ojt.management.common.payload.request.CompanyRequest;
 
 import java.io.Serializable;
 
