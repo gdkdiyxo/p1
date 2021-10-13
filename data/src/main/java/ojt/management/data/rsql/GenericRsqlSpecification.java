b7a36073baad0bea7f3c6cc6a4aa4dcefbb1e6c8
Thanh Thu Cao <thuctse63589@fpt.edu.vn>
2021-10-13 11:09:11 +0700

    make it work for nested

--- data/src/main/java/ojt/management/data/rsql/GenericRsqlSpecification.java
+++ data/src/main/java/ojt/management/data/rsql/GenericRsqlSpecification.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.List;
@@ -25,7 +26,8 @@ public class GenericRsqlSpecification<T> implements Specification<T> {
 
     @Override
     public Predicate toPredicate(final Root<T> root, final CriteriaQuery<?> query, final CriteriaBuilder builder) {
-        final List<Object> args = castArguments(root);
+        Path<String> propertyExpression = parseProperty(root);
+        List<Object> args = castArguments(propertyExpression);
         final Object argument = args.get(0);
         switch (RsqlSearchOperation.getSimpleOperator(operator)) {
 
@@ -70,21 +72,34 @@ public class GenericRsqlSpecification<T> implements Specification<T> {
 
     // === private
 
-    private List<Object> castArguments(final Root<T> root) {
-        
-        final Class<? extends Object> type = root.get(property).getJavaType();
-        
-        final List<Object> args = arguments.stream().map(arg -> {
-            if (type.equals(Integer.class)) {
-               return Integer.parseInt(arg);
-            } else if (type.equals(Long.class)) {
-               return Long.parseLong(arg);
-            } else {
-                return arg;
-            }            
-        }).collect(Collectors.toList());
+    // This method will help us diving deep into nested property using the dot convention
+    // The originial tutorial did not have this, so it can only parse the shallow properties.
+    private Path<String> parseProperty(Root<T> root) {
+        Path<String> path;
+        if (property.contains(".")) {
+            // Nested properties
+            String[] pathSteps = property.split("\\.");
+            String step = pathSteps[0];
+            path = root.get(step);
 
-        return args;
+            for (int i = 1; i <= pathSteps.length - 1; i++) {
+                path = path.get(pathSteps[i]);
+            }
+        } else {
+            path = root.get(property);
+        }
+        return path;
+    }
+
+    private List<Object> castArguments(Path<?> propertyExpression) {
+        Class<?> type = propertyExpression.getJavaType();
+
+        return arguments.stream().map(arg -> {
+            if (type.equals(Integer.class)) return Integer.parseInt(arg);
+            else if (type.equals(Long.class)) return Long.parseLong(arg);
+            else if (type.equals(Byte.class)) return Byte.parseByte(arg);
+            else return arg;
+        }).collect(Collectors.toList());
     }
 
 }
