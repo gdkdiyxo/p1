722b4049df67d9d2fbff27502c712d8cdffb4ecf
Thanh Thu Cao <katie.caooo@gmail.com>
2021-10-26 16:34:26 +0700

    WIP

--- application/src/main/java/ojt/management/controllers/UserController.java
+++ application/src/main/java/ojt/management/controllers/UserController.java
@@ -7,8 +7,7 @@ import ojt.management.business.services.AccountService;
 import ojt.management.common.exceptions.AccountIdNotExistedException;
 import ojt.management.common.payload.PagedDataResponse;
 import ojt.management.common.payload.dto.UserDTO;
-import ojt.management.common.payload.request.AccountUpdateRequest;
-import ojt.management.configuration.security.services.UserDetailsImpl;
+import ojt.management.common.payload.request.AccountRequest;
 import ojt.management.common.utils.SortUtils;
 import ojt.management.data.entities.Account;
 import ojt.management.data.rsql.CustomRsqlVisitor;
@@ -28,7 +27,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.core.Authentication;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -49,14 +47,14 @@ public class UserController {
         this.userMapper = userMapper;
     }
 
-    @PostAuthorize("hasAnyAuthority('SYS_ADMIN')")
+    @PreAuthorize("hasAnyAuthority('SYS_ADMIN')")
     @GetMapping("/{id}")
     public UserDTO getUserById(@PathVariable Long id)
             throws AccountIdNotExistedException {
         return userMapper.userToUserDTO(accountService.getUserById(id));
     }
 
-    @PostAuthorize("hasAnyAuthority('SYS_ADMIN')")
+    @PreAuthorize("hasAnyAuthority('SYS_ADMIN')")
     @GetMapping()
     public PagedDataResponse<UserDTO> searchUser(@RequestParam(value = "search", required = false) String search,
                                                  @RequestParam(value = "pageNo", required = false, defaultValue = "0") Integer pageNo,
@@ -78,15 +76,15 @@ public class UserController {
         return new PagedDataResponse<>("OK", "Retrieved account successfully.", data, pagedResult.getTotalElements(), pagedResult.getTotalPages(), pagedResult.getNumber());
     }
 
-    @PostAuthorize("hasAnyAuthority('COMPANY_REPRESENTATIVE','SYS_ADMIN', 'STUDENT')")
+    @PreAuthorize("hasAnyAuthority('COMPANY_REPRESENTATIVE','SYS_ADMIN', 'STUDENT')")
     @PutMapping("/{id}")
     public UserDTO updateUser(@PathVariable Long id,
-                              @Valid @RequestBody AccountUpdateRequest accountUpdateRequest)
+                              @Valid @RequestBody AccountRequest accountUpdateRequest)
             throws AccountIdNotExistedException {
         return userMapper.userToUserDTO(accountService.updateUser(id, accountUpdateRequest));
     }
 
-    @PostAuthorize("hasAnyAuthority('SYS_ADMIN')")
+    @PreAuthorize("hasAnyAuthority('SYS_ADMIN')")
     @DeleteMapping("/{id}")
     public boolean deleteUser(@PathVariable Long id)
             throws AccountIdNotExistedException {
